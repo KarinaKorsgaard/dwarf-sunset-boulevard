22237e99fc6820e06b968f143f609f9269b14a3e
KarinaKorsgaard <kkorsgaardjensen@gmail.com>
2019-10-16 14:01:07 +0200

    added burger for mobile view

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -1,23 +1,38 @@
 <template>
-  <nav>
+  <nav v-if="!isSmall">
     <img class="logo" :src="require('../assets/logo.svg')" />
     <router-link :to="{ name: 'home' }">Restaurenter</router-link>
     <router-link :to="{ name: 'home' }">Produkter</router-link>
     <router-link :to="{ name: 'home' }">Nyhedsbrev</router-link>
     <router-link :to="{ name: 'home' }">Kontakt</router-link>
   </nav>
+  <nav v-else>
+    <img class="logo" :src="require('../assets/logo.svg')" />
+    <img class="burger" :src="require('../assets/burger.png')" />
+  </nav>
 </template>
 
 <script>
 export default {
-  name: "TopNavigation"
+  name: "TopNavigation",
+  data: function() {
+    return {
+      isSmall: false
+    };
+  },
+  mounted() {
+    this.resize();
+    window.onresize = this.resize;
+  },
+  methods: {
+    resize() {
+      this.isSmall = document.body.clientWidth < 399;
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.logo {
-  width: 50px;
-}
 nav {
   position: fixed;
   left: 0;
@@ -27,7 +42,10 @@ nav {
   height: 70px;
   background: white;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  @include medium {
+    justify-content: space-around;
+  }
 
   font-size: 0.7em;
   line-height: 70px;
@@ -36,6 +54,17 @@ nav {
     font-weight: bold;
     text-decoration: none;
     text-transform: uppercase;
+  }
+  .logo {
+    width: 50px;
+    margin: 10px;
+  }
+  .burger {
+    height: 20px;
+    width: 30px;
+    align-self: center;
+    margin: 10px;
+    cursor: pointer;
   }
 }
 </style>
